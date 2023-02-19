d45a174641fe56dedb43bd9dfa6b81ec9a5b32a5
David Senack <david.senack@gmail.com>
2023-02-18 22:44:47 -0500

    Add 'view more' button functionality and refactor app.js code

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-// Search update functionallity
+// Set global variable for search term
 var prevSearchTerm = "nature";
 
-function rep() {
+// Function to run on page load and new search
+function pageInit() {
+  // Do something.
+}
+
+// Function to run get results for new search
+function newSearch() {
   let newSearchTerm = document.getElementById("search-bar").value;
   document.body.innerHTML
       = document.body.innerHTML
@@ -9,6 +15,17 @@ function rep() {
   prevSearchTerm = newSearchTerm;
 }
 
-// View more button functionality
+// Function to run on view more button click
+function viewMore() {
+  for (let i = 0; i < 36; i++) {
+    let imgNum = Math.floor(Math.random() * 1000);
+    let newImg = document.createElement("img");
+    newImg.src = "https://source.unsplash.com/random/1080x1080/?" + prevSearchTerm + "/" + imgNum;
+    newImg.className = "grid-img";
+    newImg.alt = "unsplash image";
+    document.getElementById("image-grid-container").appendChild(newImg);
+  }
+}
 
-
+// Event listener on click of search button
+document.getElementById("view-more-btn").onclick = viewMore();
