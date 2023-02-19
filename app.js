768edbab48f33d48a2beb0917f746a85f1965d79
David Senack <david.senack@gmail.com>
2023-02-18 20:03:12 -0500

    Remove internal javascript and place in external app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-// ON SEARCH BUTTON CLICK 
+// Search update functionallity
+var prevSearchTerm = "nature";
 
-// document.querySelectorAll
-// function newSearch(str, find, replace) {
-//     return str.replace(find, replace);
-// }
-
-function generateSearchResult() {
-  let search = document.getElementById("search-bar").value;
-  document.replaceAll("nature", search)
-  console.log(search);
+function rep() {
+  let newSearchTerm = document.getElementById("search-bar").value;
+  document.body.innerHTML
+      = document.body.innerHTML
+      .replaceAll(prevSearchTerm, newSearchTerm);
+  prevSearchTerm = newSearchTerm;
 }
+
+// View more button functionality
+
+
