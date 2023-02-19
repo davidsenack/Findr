65c93f112db457baf3609978b4f661f0ab4acc05
David Senack <david.senack@gmail.com>
2023-02-19 13:15:57 -0500

    Add linting to app.js using AirBnB style guide

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
 // GLOBAL VARIABLES
-var currentSearchStr = ["nature"];
-
+let currentSearchStr = ['nature'];
 
 // FUNCTIONS
-
-// Function to run on page load and new search
-// Removes all children of image grid container and initializes new image grid
-function pageInit() {
-  removeChildren(document.getElementById("image-grid-container"));
-  viewMore();
-};
-
-// Function to get results for new search
-// Clears image grid and runs pageInit function with new 
-function newSearch() {
-  let originalSearchString = document.getElementById("search-bar").value;
-  currentSearchStr = originalSearchString.split(" ").join("+");
-  pageInit();
-};
-
-// Function to run on view more button click
-// Creates 36 new images and appends them to the image grid container
-function viewMore() {
-  for (let i = 0; i < 72; i++) {
-    let imgNum = Math.floor(Math.random() * 10000);
-    let newImg = document.createElement("img");
-
-    newImg.src = "https://source.unsplash.com/random/1080x1080/?" + currentSearchStr + "/" + imgNum;
-    newImg.className = "grid-img";
-    newImg.alt = "unsplash image";
-    document.getElementById("image-grid-container").appendChild(newImg);
-  };
-};
 
 // Function to remove all children of a parent element
 function removeChildren(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
-  };
-};
+  }
+}
+
+// Function to run on view more button click
+// Creates 36 new images and appends them to the image grid container
+function viewMore() {
+  for (let i = 0; i < 72; i += 1) {
+    const imgNum = Math.floor(Math.random() * 10000);
+    const newImg = document.createElement('img');
+
+    newImg.src = `https://source.unsplash.com/random/1080x1080/?${currentSearchStr}/${imgNum}`;
+    newImg.className = 'grid-img';
+    newImg.alt = 'unsplash image';
+    document.getElementById('image-grid-container').appendChild(newImg);
+  }
+}
+
+// Function to run on page load and new search
+// Removes all children of image grid container and initializes new image grid
+function pageInit() {
+  removeChildren(document.getElementById('image-grid-container'));
+  viewMore();
+}
+
+// Function to get results for new search
+// Clears image grid and runs pageInit function with new
+// eslint-disable-next-line no-unused-vars
+function newSearch() {
+  const originalSearchString = document.getElementById('search-bar').value;
+  currentSearchStr = originalSearchString.split(' ').join('+');
+  pageInit();
+}
 
 // Functions to shrink header on scroll
 function smallHeader() {
-  document.querySelector("header").style.height = "0px";
-  document.getElementById("header-logo").style.height = "35px";
-  document.querySelector("header").style.backgroundColor = "rgba(14, 103, 180, 0.8)";
+  document.querySelector('header').style.height = '0px';
+  document.getElementById('header-logo').style.height = '35px';
+  document.querySelector('header').style.backgroundColor = 'rgba(14, 103, 180, 0.8)';
 }
 
 function largeHeader() {
-  document.querySelector("header").style.height = "70px";
-  document.getElementById("header-logo").style.height = "50px";
-  document.querySelector("header").style.backgroundColor = "#0E67B4";
+  document.querySelector('header').style.height = '70px';
+  document.getElementById('header-logo').style.height = '50px';
+  document.querySelector('header').style.backgroundColor = '#0E67B4';
 }
 
 function scrollFunction() {
@@ -58,8 +58,8 @@ function scrollFunction() {
     smallHeader();
   } else {
     largeHeader();
-  };
-};
+  }
+}
 
 // EVENT LISTENERS
 
@@ -67,7 +67,7 @@ function scrollFunction() {
 window.onload = pageInit();
 
 // Listen for 'view more' button click
-document.getElementById("view-more-btn").onclick = function() {viewMore()};
+document.getElementById('view-more-btn').onclick = function viewMoreEvent() { viewMore(); };
 
 // Listen for scroll event
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function scrollEvent() { scrollFunction(); };
